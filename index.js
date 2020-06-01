0813892deeffb72e94dcfab85ecad4f5c3f2fdf1
pore0007 <65929893+pore0007@users.noreply.github.com>
2020-06-02 01:18:35 +0530

    JS file

---
+++ index.js
@@ -0,0 +1,49 @@
+let a = 3;
+let b = 5;
+let c;
+
+let output1; 
+// task description
+output1 = 'let a = 3;\n';
+output1 += 'let b = 5;\n';
+output1 += 'let c;\n';
+output1 += '---------\n';
+
+// using operators
+output1 += 'a + b = ' + (a + b) + '\n';
+output1 += 'a - b = ' + (a - b) + '\n';
+output1 += 'a * b = ' + (a * b) + '\n';
+output1 += 'a / b = ' + (a / b) + '\n';
+output1 += 'a % b = ' + (a % b) + '\n';
+output1 += 'a += b = ' + (a += b) + '\n';
+output1 += 'a -= b = ' + (a -= b) + '\n';
+output1 += 'a *= b = ' + (a *= b) + '\n';
+output1 += 'a /= b = ' + (a /= b) + '\n';
+output1 += 'a %= b = ' + (a %= b) + '\n';
+output1 += 'a == b ; ' + (a == b) + '\n';
+output1 += 'a != b ; ' + (a != b) + '\n';
+output1 += 'a > b ; ' + (a > b) + '\n';
+output1 += 'a < b ; ' + (a < b) + '\n';
+output1 += '!a && !c ; ' + (!a && !c ) + '\n';
+output1 += '!a || !c ; ' + (!a || !c) + '\n';
+
+alert(output1);
+
+//sentence
+let first_name = 'Ridima';
+let last_name = 'Pore';
+let mail = 'pore0007@algonquinlive.com';
+
+let output;
+output = '"My name is ';
+output += first_name;
+output += ' ';
+output += last_name;
+output += '.';
+output += ' ';
+output += 'You can contact me at '
+output += mail;
+output += '."'; 
+
+console.log(output);
+
